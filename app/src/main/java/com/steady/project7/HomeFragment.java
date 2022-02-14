5e9baca31065b43a62f0baa8b2e57948350e3d4c
steady00 <steady.tjiakra@gmail.com>
2022-02-14 17:12:45 +0700

    Revisi by Steady

--- app/src/main/java/com/steady/project7/HomeFragment.java
+++ app/src/main/java/com/steady/project7/HomeFragment.java
@@ -8,16 +8,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.loader.content.AsyncTaskLoader;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -26,59 +30,109 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class HomeFragment extends Fragment implements View.OnClickListener {
-    Button btn_home_instruktur, btn_home_peserta, btn_home_materi, btn_home_kelas, btn_search_peserta, btn_search_jumlah_peserta;
+    Button btn_search_peserta, btn_search_jumlah_peserta;
     ListView list_search1;
     TextView txt_search3;
-    EditText edit_serach_peserta, edit_serach_jumlah_peserta;
+    EditText edit_serach_jumlah_peserta;
+    Spinner edit_search_peserta;
     private String JSON_STRING;
     private String JSON_STRING_JML;
+    private String JSON_STRING_MAT;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        btn_home_instruktur =  view.findViewById(R.id.btn_home_instruktur);
-        btn_home_peserta =  view.findViewById(R.id.btn_home_peserta);
-        btn_home_materi =  view.findViewById(R.id.btn_home_materi);
-        btn_home_kelas =  view.findViewById(R.id.btn_home_kelas);
-
         btn_search_peserta = view.findViewById(R.id.btn_search_peserta);
         list_search1 = view.findViewById(R.id.list_search1);
-        edit_serach_peserta = view.findViewById(R.id.edit_search_peserta);
+        edit_search_peserta = (Spinner) view.findViewById(R.id.edit_search_peserta);
 
         btn_search_jumlah_peserta = view.findViewById(R.id.btn_search_jumlah_peserta);
         txt_search3 =view.findViewById(R.id.txt_search3);
         edit_serach_jumlah_peserta = view.findViewById(R.id.edit_search_jumlah_peserta);
 
-        btn_home_instruktur.setOnClickListener(this);
-        btn_home_peserta.setOnClickListener(this);
-        btn_home_materi.setOnClickListener(this);
-        btn_home_kelas.setOnClickListener(this);
         btn_search_peserta.setOnClickListener(this);
         btn_search_jumlah_peserta.setOnClickListener(this);
+
+        //dropdownList();
 
         return view;
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        dropdownList();
+    }
+
+    @Override
     public void onClick(View view) {
-        if (view == btn_home_instruktur){
-            startActivity(new Intent(getActivity(), MainActivity.class).putExtra("keyName", "instruktur"));
-        } else if ( view == btn_home_peserta){
-            startActivity(new Intent(getActivity(), MainActivity.class).putExtra("keyName", "peserta"));
-        } else if (view == btn_home_materi){
-            startActivity(new Intent(getActivity(), MainActivity.class).putExtra("keyName", "materi"));
-        }else if (view == btn_home_kelas){
-            startActivity(new Intent(getActivity(), MainActivity.class).putExtra("keyName", "kelas"));
-        }else if (view == btn_search_peserta) {
+        if (view == btn_search_peserta) {
             getJSON();
         }else if (view == btn_search_jumlah_peserta) {
             getJSONJmlPeserta();
         }
     }
 
+    private void dropdownList() {
+        // bantuan dari class AsyncTask
+        class DropdownList extends AsyncTask<Void, Void, String> {
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(getActivity(),
+                        "Mengambil Data", "Harap menunggu...",
+                        false, false);
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                HttpHandler handler = new HttpHandler();
+                String materi = handler.sendGetResponse(Konfigurasi.URL_GET_ALL_MATERI);
+                return materi;
+            }
+
+            @Override
+            protected void onPostExecute(String message) {
+                super.onPostExecute(message);
+                loading.dismiss();
+
+                JSON_STRING_MAT = message;
+                JSONObject jsonObjectMat = null;
+                ArrayList<String> arrayListMat = new ArrayList<>();
+                try {
+                    //json array data detail kelas
+                    jsonObjectMat = new JSONObject(JSON_STRING_MAT);
+                    JSONArray jsonArray = jsonObjectMat.getJSONArray(Konfigurasi.TAG_JSON_ARRAY);
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject nama_materi = jsonArray.getJSONObject(i);
+                        String nameMateri = nama_materi.getString(Konfigurasi.TAG_JSON_NAMA_MAT);
+
+                        arrayListMat.add(nameMateri);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                Log.d("Materi", String.valueOf(arrayListMat));
+                ArrayAdapter<String> adapter_materi = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, arrayListMat);
+                adapter_materi.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                edit_search_peserta.setAdapter(adapter_materi);
+
+            }
+        }
+        DropdownList dropdownList = new DropdownList();
+        dropdownList.execute();
+    }
+
     private void getJSONJmlPeserta() {
-        String tahun_mulai = edit_serach_jumlah_peserta.getText().toString().trim();
+        final String tahun_mulai = edit_serach_jumlah_peserta.getText().toString().trim();
         class GetJSONJmlPeserta extends AsyncTask<Void, Void, String>
         {
             ProgressDialog loading;
@@ -132,7 +186,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     }
 
     private void getJSON() {
-        String nama_materi = edit_serach_peserta.getText().toString().trim();
+        String nama_materi = edit_search_peserta.getSelectedItem().toString().trim();
         class GetJSON extends AsyncTask<Void, Void, String>
         {
             ProgressDialog loading;
